a4f24d47c27bef59422002c7ee06a20283f91c42
Harsh Kumar <harsh.kumar@myhealthcare.life>
2025-07-11 12:02:22 +0530

    fix

--- src/pages/UpdateProfile.jsx
+++ src/pages/UpdateProfile.jsx
@@ -74,128 +74,132 @@ const UpdateProfile = () => {
   ];
 
   return (
-    <div className="container d-flex justify-content-center align-items-center minHeight">
-      <div className="row">
+    <div className="container minHeight">
+      <div className="row my-4">
         <div className="col-12">
           <BreadCrumb items={breadcrumbItems} />
         </div>
       </div>
-      <div
-        className="card p-4 shadow-lg mt-4"
-        style={{ width: "100%", maxWidth: "400px" }}
-      >
-        <h4 className="mb-3 text-center">Update</h4>
-        <form onSubmit={handleUpdate}>
-          <div className="mb-3">
-            <label htmlFor="email" className="form-label">
-              Email address
-            </label>
-            <input
-              type="email"
-              className="form-control"
-              name="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              tabIndex={1}
-            />
-          </div>
+      <div className="row">
+        <div className="col-12 d-flex justify-content-center">
+          <div
+            className="card p-4 shadow-lg"
+            style={{ width: "100%", maxWidth: "400px" }}
+          >
+            <h4 className="mb-3 text-center">Update</h4>
+            <form onSubmit={handleUpdate}>
+              <div className="mb-3">
+                <label htmlFor="email" className="form-label">
+                  Email address
+                </label>
+                <input
+                  type="email"
+                  className="form-control"
+                  name="email"
+                  id="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  tabIndex={1}
+                />
+              </div>
 
-          <div className="mb-3">
-            <label htmlFor="name" className="form-label">
-              Name
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              name="name"
-              id="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              tabIndex={2}
-            />
-          </div>
+              <div className="mb-3">
+                <label htmlFor="name" className="form-label">
+                  Name
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="name"
+                  id="name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  tabIndex={2}
+                />
+              </div>
 
-          <div className="mb-3 position-relative">
-            <label htmlFor="password" className="form-label">
-              Password
-            </label>
-            <input
-              type={showPassword ? "text" : "password"}
-              className="form-control"
-              name="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              tabIndex={3}
-            />
-            <span
-              style={{
-                position: "absolute",
-                top: "38px",
-                right: "15px",
-                cursor: "pointer",
-              }}
-              onClick={() => setShowPassword((prev) => !prev)}
-            >
-              {showPassword ? <FaRegEyeSlash /> : <FaRegEye />}
-            </span>
-          </div>
+              <div className="mb-3 position-relative">
+                <label htmlFor="password" className="form-label">
+                  Password
+                </label>
+                <input
+                  type={showPassword ? "text" : "password"}
+                  className="form-control"
+                  name="password"
+                  id="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  tabIndex={3}
+                />
+                <span
+                  style={{
+                    position: "absolute",
+                    top: "38px",
+                    right: "15px",
+                    cursor: "pointer",
+                  }}
+                  onClick={() => setShowPassword((prev) => !prev)}
+                >
+                  {showPassword ? <FaRegEyeSlash /> : <FaRegEye />}
+                </span>
+              </div>
 
-          <div className="mb-3">
-            <label htmlFor="role" className="form-label">
-              Role
-            </label>
-            <select
-              className="form-select"
-              name="role"
-              id="role"
-              value={role}
-              onChange={(e) => setRole(e.target.value)}
-              tabIndex={4}
-            >
-              <option value="">Select Role</option>
-              <option value="admin">Admin</option>
-              <option value="customer">Customer</option>
-            </select>
-          </div>
+              <div className="mb-3">
+                <label htmlFor="role" className="form-label">
+                  Role
+                </label>
+                <select
+                  className="form-select"
+                  name="role"
+                  id="role"
+                  value={role}
+                  onChange={(e) => setRole(e.target.value)}
+                  tabIndex={4}
+                >
+                  <option value="">Select Role</option>
+                  <option value="admin">Admin</option>
+                  <option value="customer">Customer</option>
+                </select>
+              </div>
 
-          <div className="mb-3">
-            <label htmlFor="avatar" className="form-label">
-              Avatar URL
-            </label>
-            <input
-              type="url"
-              className="form-control"
-              name="avatar"
-              id="avatar"
-              value={avatar}
-              onChange={(e) => setAvatar(e.target.value)}
-              tabIndex={5}
-            />
-          </div>
+              <div className="mb-3">
+                <label htmlFor="avatar" className="form-label">
+                  Avatar URL
+                </label>
+                <input
+                  type="url"
+                  className="form-control"
+                  name="avatar"
+                  id="avatar"
+                  value={avatar}
+                  onChange={(e) => setAvatar(e.target.value)}
+                  tabIndex={5}
+                />
+              </div>
 
-          <div className="d-flex justify-content-center">
-            <button
-              type="submit"
-              className="btn btn-outline-primary px-3 rounded-5"
-              disabled={loading}
-            >
-              {loading ? (
-                <>
-                  <span
-                    className="spinner-border spinner-border-sm me-2"
-                    role="status"
-                    aria-hidden="true"
-                  ></span>
-                  Updateing Profile...
-                </>
-              ) : (
-                "Update Profile"
-              )}
-            </button>
+              <div className="d-flex justify-content-center">
+                <button
+                  type="submit"
+                  className="btn btn-outline-primary px-3 rounded-5"
+                  disabled={loading}
+                >
+                  {loading ? (
+                    <>
+                      <span
+                        className="spinner-border spinner-border-sm me-2"
+                        role="status"
+                        aria-hidden="true"
+                      ></span>
+                      Updateing Profile...
+                    </>
+                  ) : (
+                    "Update Profile"
+                  )}
+                </button>
+              </div>
+            </form>
           </div>
-        </form>
+        </div>
       </div>
     </div>
   );
